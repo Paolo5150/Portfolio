83fa3d1052980532a5ef61c1f20b81b373de3ca0
Paolo_X86 <p.ferri1986@gmail.com>
2020-11-12 16:52:17 +0800

    Minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,76 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let texttt = '';
+
+let address = process.env.dialogFlow || 'https://pf-chatbot.herokuapp.com/dialogflow-in';
+
+export class App extends React.Component {
+
+  setQuery = (text:string) => 
+  {
+    texttt = text;
+  }
+
+  test = () => 
+  {
+    const req = {
+      queryInput: {
+       /* event: {
+          "name": 'Welcome',
+          "languageCode": 'en-US'
+        },*/
+        text: {          
+          text: texttt,
+          languageCode: 'en-US',          
+        },
+      },
+    };
+
+
+    fetch(address, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(req)
+  }).then((res) => { 
+    return res.text()
+  })
+  .then((jsonData) => {
+    console.log(jsonData);
+  })
+  .catch((err) => {
+    // handle error for example
+    console.error(err);
+  });
+   
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p >
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <input type="text" onChange={event => this.setQuery(event.target.value)}></input>
+          <button style={{width: '100px', height: '100px'}} onClick={this.test}>Click</button>
+
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
