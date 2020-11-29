1843c187a4d69b1b2e069eeac90627cdf3ce5856
Paolo5150 <p.ferri1986@gmail.com>
2020-11-30 06:51:44 +0800

    Style changes

--- src/components/ProjectThumb.tsx
+++ src/components/ProjectThumb.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import ScrollAnimation from 'react-animate-on-scroll';
 import { Container, Row, Col } from 'react-bootstrap';
 import '../css/index.css'
@@ -23,6 +23,7 @@ export class ProjectThumb extends React.Component<Props> {
         };
       }
 
+      private buttonRef = createRef<HTMLDivElement>();
 
       renderIcons = ()=>{
           return (
@@ -36,31 +37,45 @@ export class ProjectThumb extends React.Component<Props> {
           </div>)
       }
 
+      btnStyle = {
+          width : '100%'
+      }
+
     render() {
         return (
             <ScrollAnimation    delay={5}
             animateIn="slideInUp">
-            <div className="project-thumb-container" style={{padding: '50px', margin: '15px', borderRadius: '20px'}} > 
+            <div className="project-thumb-container" style={{padding: '10px', margin: '15px', borderRadius: '20px'}}> 
                <Container fluid>
                         <Row >
-                            <Col xs="12" lg="12" style={{textAlign: 'left'}}>
+                            <Col xs="12" lg="12" >
                                 <div style={{width: this.props.width, maxWidth: '100%', margin: 'auto'}} >
                                     <img src={this.props.imgSrc} width='100%' alt="Thumb" style={{objectFit: 'contain'}}></img>
                                 </div>
                             </Col>
-                            <Col xs="12" lg="12" style={{textAlign: 'left', fontFamily: 'basic'}}>
-                                <h3 style={{textAlign: 'center', marginTop: '20px', marginBottom: '20px', fontWeight: 'bold'}}>{this.props.title}</h3>
+                        </Row>
+                        <Row style={{ fontFamily: 'basic'}}>
+                        <h3 style={{textAlign: 'center',marginTop: '20px', marginBottom: '20px', fontWeight: 'bold', width: '100%'}}>{this.props.title}</h3>
+
+                        </Row>
+                        <Row>
+                        <Col xs="12" lg="12" style={{textAlign: 'left', fontFamily: 'basic'}}>
                                 <div style={{margin: 'auto', textAlign: 'center'}}>
                                 {
                                     this.renderIcons()
                                 }
                                 </div>
-                                <p style={{fontSize: '1.3em', paddingTop: '20px', paddingBottom: '20px', marginBottom: '40px'}}>{this.props.description}</p>
-                                <button  onClick={()=>{this.props.clickCallback(this.props.id)}}
-                                    style={{position: 'absolute', bottom: 0, width: '80px', height: '30px'}}>View</button>
+                                <p style={{fontSize: '1.3em', paddingTop: '20px', paddingBottom: '20px', marginBottom: '30px'}}>{this.props.description}</p>
+                                <div style={{textAlign: 'center'}} ref={this.buttonRef}>
+                                    <button   onClick={()=>{this.props.clickCallback(this.props.id)}}
+                                        style={{ bottom: 0, width: '80px', height: '30px', borderRadius: '20px'}}>View</button>
+
+                                </div>
 
                             </Col>
                         </Row>
+                           
+                
                     </Container>      
             </div>
             </ScrollAnimation>
